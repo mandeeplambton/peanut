3024b983a2efdbb901a285931f0c19b910ce7b99
visualstudio <visualstudio@230-PC45>
2019-03-28 16:22:16 -0400

    Anonymas Method

---
+++ C0730387/C0730387/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mandeep kaur rules the IT universe"); Download();
+        }
+
+        static void Download()
+        {
+            Task.Run(() =>
+            {
+                Thread.Sleep(3000);
+                Console.WriteLine("Download Complete");
+            });
+
+
+        }
+    }
+}
---
+++ sidhumani/sidhumani/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace sidhumani
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello");
+        }
+        static void download()
+        {
+            Thread.Sleep(7000);
+            Console.WriteLine("download complete");
+        }
+    }
+}
